9d3e50838a67f76c473c86266fa09a636d07c631
Heneli Kailahi <heneli@kailahi.com>
2023-06-18 11:09:24 -0700

    Lint. Rearrange CSS properties

--- heneli-dot-dev/layouts/PostLayout.tsx
+++ heneli-dot-dev/layouts/PostLayout.tsx
@@ -120,7 +120,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose prose-lg sm:prose-base max-w-none pb-8 pt-10 dark:prose-dark">{children}</div>
+              <div className="prose prose-lg max-w-none pb-8 pt-10 dark:prose-dark sm:prose-base">
+                {children}
+              </div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={twitterUrl(path)} rel="nofollow">
                   Discuss on Twitter
