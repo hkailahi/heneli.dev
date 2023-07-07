79e778ec9541097966210b893df90950c7c15203
Heneli Kailahi <heneli@kailahi.com>
2023-07-07 16:56:33 -0700

    Fix GH and Twt links

--- heneli-dot-dev/layouts/PostSimple.tsx
+++ heneli-dot-dev/layouts/PostSimple.tsx
@@ -17,9 +17,9 @@ interface LayoutProps {
   prev?: { path: string; title: string }
 }
 
-const editUrl = (path) => `${siteMetadata.siteRepo}/blob/master/heneli-dot-dev/data/${path}`
+const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/heneli-dot-dev/data/${path}`
 const twitterUrl = (path) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
+  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`.replace(/^https:\/\//, ''))}`
 const issueUrl = `https://github.com/hkailahi/heneli.dev/issues`
 const discussUrl = `https://github.com/hkailahi/heneli.dev/discussions`
 const trashUrl = `https://news.ycombinator.com/`
