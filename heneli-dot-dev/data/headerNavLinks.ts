9f5b079f93ffee6e8c21338c8a47afb25df5e928
Heneli Kailahi <heneli@kailahi.com>
2024-02-01 17:18:52 -0800

    Add CV section and host resume on site. Remove Tags from nav for now until it's useful

--- heneli-dot-dev/data/headerNavLinks.ts
+++ heneli-dot-dev/data/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/tags', title: 'Tags' },  # bring back once there are more posts
   // { href: '/projects', title: 'Projects' },
   // { href: '/drafts', title: 'Blog Drafts' }, // anti-instance, intentionally unlinked
   { href: '/feed.xml', title: 'RSS' },
+  { href: '/static/files/Heneli Kailahi - Resume 2024.pdf', title: 'CV' },
   { href: '/about', title: 'About' },
 ]
 
